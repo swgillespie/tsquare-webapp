c6eff05c7d13123918f6c1d78277b8142726d38b
swgillespie <sean.william.g@gmail.com>
2013-10-11 19:00:26 -0700

    stuff

--- CS_4911_Tsquare/settings.py
+++ CS_4911_Tsquare/settings.py
@@ -15,17 +15,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tsqdb',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
