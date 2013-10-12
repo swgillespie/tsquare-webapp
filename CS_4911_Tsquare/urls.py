d575109cfa2f52e32a833d0ccfc017712516ba53
swgillespie <sean.william.g@gmail.com>
2013-10-11 18:42:35 -0700

    heroku

--- CS_4911_Tsquare/urls.py
+++ CS_4911_Tsquare/urls.py
@@ -7,16 +7,16 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
    
     # Examples:
-    url(r'^$','tsquare.views.index'),
-    url(r'^tlogin/$', 'tsquare.views.tlogin'),
-    url(r'^tlogout/$','tsquare.views.tlogout'),
-    url(r'^home/$','tsquare.views.home'),
-    url(r'^setup_profile/$','tsquare.views.setup_profile'), 
-    url(r'^github/repos/select/$','tsquare.views.select_github_repos'),
-    url(r'^github_login/$','tsquare.views.github_login'),
-    url(r'^github_login_exchange/$','tsquare.views.github_login_exchange'),
-    url(r'^list_assignments/$','tsquare.views.list_assignments')
-    # url(r'^CS_4911_Tsquare/', include('CS_4911_Tsquare.foo.urls')),
+    url(r'^$','tsquare-web.views.index'),
+    url(r'^tlogin/$', 'tsquare-web.views.tlogin'),
+    url(r'^tlogout/$','tsquare-web.views.tlogout'),
+    url(r'^home/$','tsquare-web.views.home'),
+    url(r'^setup_profile/$','tsquare-web.views.setup_profile'), 
+    url(r'^github/repos/select/$','tsquare-web.views.select_github_repos'),
+    url(r'^github_login/$','tsquare-web.views.github_login'),
+    url(r'^github_login_exchange/$','tsquare-web.views.github_login_exchange'),
+    url(r'^list_assignments/$','tsquare-web.views.list_assignments')
+    # url(r'^CS_4911_Tsquare/', include('CS_4911_Tsquare-Web.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
