0edacd10910de0cd8a796f476d4b00248a6a3627
camreon <cguthrie7@gatech.edu>
2013-10-24 18:21:45 -0400

    Added simple views and URLs for the remaining pages we are going to implement, made the index page automatically redirect to the login page, made the layout of all pages more constant

--- tsquare-web/views.py
+++ tsquare-web/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
 from django.shortcuts import render,render_to_response,redirect
-from tsquare.core import TSquareAPI, TSquareAuthException
+from tsquare_api import TSquareAPI, TSquareAuthException
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -58,10 +58,6 @@ def profile(request):
 	return render_to_response('profile.html')
 
 @login_required
-def assignments(request):
-	return render_to_response('assignments.html')
-
-@login_required
 def resources(request):
 	return render_to_response('resources.html')
 
@@ -115,3 +111,19 @@ def list_assignments(request):
 	for s in sites:
 		assignments.append(tsapi.get_assignments(s))
 	return HttpResponse(s)
+
+@login_required
+def course_info(request):
+	return render_to_response('course_info.html')
+
+@login_required
+def announcements(request):
+	return render_to_response('announcements.html')
+
+@login_required
+def wiki(request):
+	return render_to_response('wiki.html')
+
+@login_required
+def help(request):
+	return render_to_response('help.html')
